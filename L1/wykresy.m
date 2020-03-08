c002d8f6e02997e77cb170e43cc55e64667c9718
BuildTools <unconfigured@null.spigotmc.org>
2020-03-08 23:20:11 +0100

    another commmit

---
+++ L1/wykresy.m
@@ -0,0 +1,17 @@
+clc
+clear
+close
+
+load("wyniki.txt");
+
+plot(wyniki(:,1),wyniki(:,2));
+figure;
+plot(wyniki(:,1),wyniki(:,3));
+figure;
+plot(wyniki(:,1),wyniki(:,4));
+figure;
+plot(wyniki(:,1),wyniki(:,5));
+figure;
+plot(wyniki(:,1),wyniki(:,6));
+figure;
+plot(wyniki(:,1),wyniki(:,7));
